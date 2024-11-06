0ed8dbe607150e21dd1d8206744b9c104b47a998
Cristian St.Clair <cristianstclair@gmail.com>
2024-11-06 13:07:03 -0800

    feat: Added text box to homepage

--- web-forum/src/App.js
+++ web-forum/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+// import 'devextreme/dist/css/dx.light.css';
+// import { TextBox } from 'devextreme-react/text-box';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to The Brink
         </p>
         <a
           className="App-link"
@@ -17,6 +26,12 @@ function App() {
         >
           Learn React
         </a>
+
+        <div>
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+          <p>You typed: {inputValue}</p>
+        </div>
+        
       </header>
     </div>
   );
