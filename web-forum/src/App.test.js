5cecc9b0120e1741137b6b8e2a31825e0120edd7
Cristian St.Clair <cristianstclair@gmail.com>
2024-11-13 14:13:25 -0800

    test: Added test for MainTextbox and Send Post Buttons

--- web-forum/src/App.test.js
+++ web-forum/src/App.test.js
@@ -1,8 +1,46 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import MainTextbox from './MainTextbox';
+import PostButton from './PostButton';
+import userEvent from '@testing-library/user-event';
+import { fireEvent } from '@testing-library/react';
+import { handleClick } from './PostButton'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Post Textbox', () => {
+  test('renders textbox for new posts', () => {
+    render(<MainTextbox />)
+    const textboxElement = screen.getByRole('textbox');
+    expect(textboxElement).toBeInTheDocument();
+  });
+
+  test('updates the value on change', () => {
+    render(<MainTextbox />)
+    const textboxElement = screen.getByRole('textbox');
+    userEvent.type(textboxElement, 'Hello World');
+    expect(textboxElement.value).toBe('Hello World');
+  });
+
+});
+
+describe('Post Button', () => {
+  test('renders green "Send Post" button', () => {
+    render(<PostButton />)
+    const postButton = screen.getByRole('button');
+    expect(postButton).toBeInTheDocument();
+  })
+    
+  // Currently having issues when running this test. Can't mock "handleClick" correctly.
+  test('calls onClick handler when clicked', () => {
+    jest.mock('./PostButton' , () => {
+      return {
+        leaveBreadcrumb: jest.fn(),
+      };
+    });
+    handleClick.mockReturnValue("New Post")
+
+    render(<PostButton label="Send Post" onClick={handleClick} />);
+    const postButton = screen.getByText("Send Post");
+    fireEvent.click(postButton);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  })
 });
